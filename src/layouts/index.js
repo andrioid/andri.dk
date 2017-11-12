350f524f0ad0aa915401218a8af14e82992999d4
Andri Óskarsson <andri80@gmail.com>
2017-11-12 11:24:48 +0100

    initial

---
+++ src/layouts/index.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import Link from 'gatsby-link'
+import Helmet from 'react-helmet'
+
+// import './index.css'
+import 'semantic-ui-css/semantic.min.css'
+
+const TemplateWrapper = ({ children }) => (
+  <div>
+    <Helmet
+      title="Gatsby Default Starter"
+      meta={[
+        { name: 'description', content: 'Sample' },
+        { name: 'keywords', content: 'sample, something' },
+      ]}
+    />
+    <div
+      className="ui grid container"
+    >
+      {children()}
+    </div>
+  </div>
+)
+
+TemplateWrapper.propTypes = {
+  children: PropTypes.func,
+}
+
+export default TemplateWrapper
