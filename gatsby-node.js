20c9ae1f407a0dca43baf84c60d00b89bf2750cb
Andri Óskarsson <andri80@gmail.com>
2017-11-19 10:03:55 +0100

    wip

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,17 +1,17 @@
 // Implement the Gatsby API “onCreatePage”. This is
 // called after every page is created.
 exports.onCreatePage = async ({ page, boundActionCreators }) => {
-    const { createPage } = boundActionCreators
-  
-    return new Promise((resolve, reject) => {
-      if (page.path.match(/^\/$/)) {
-        // It's assumed that `landingPage.js` exists in the `/layouts/` directory
-        page.layout = "plain"
-  
-        // Update the page.
-        createPage(page)
-      }
-  
-      resolve()
-    })
-  }
+	const { createPage } = boundActionCreators
+
+	return new Promise((resolve, reject) => {
+		if (page.path.match(/^\/mooo$/)) {
+			// It's assumed that `landingPage.js` exists in the `/layouts/` directory
+			page.layout = 'plain'
+
+			// Update the page.
+			createPage(page)
+		}
+
+		resolve()
+	})
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,12 +2,15 @@ import React from 'react'
 import Link from 'gatsby-link'
 
 const IndexPage = () => (
-  <div className="ui column" >
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <Link to="/page-2/">Go to page 2</Link>
-  </div>
+	<div className="ui column">
+		<h1>Andri frontpage</h1>
+		<ul>
+			<li>Github</li>
+			<li>Twitter</li>
+			<li>LinkedIn</li>
+		</ul>
+		<Link to="/page-2/">Go to page 2</Link>
+	</div>
 )
 
 export default IndexPage
