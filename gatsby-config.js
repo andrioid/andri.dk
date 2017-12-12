e6ff0a37e9bb5c398bb01613af5f2bc74d1c7bc0
Andri Óskarsson <andri80@gmail.com>
2017-12-12 12:50:26 +0100

    trying to get source rss working

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,6 +7,13 @@ module.exports = {
 			resolve: 'gatsby-source-filesystem',
 			options: { name: 'src', path: `${__dirname}/src/` }
 		},
+		{
+			resolve: 'gatsby-source-rss',
+			options: {
+				rssURL:
+					'https://feeds.pinboard.in/rss/secret:16b24ead131e0282c0a9/u:andrioid/'
+			}
+		},
 		'gatsby-transformer-remark',
 		`gatsby-plugin-react-helmet`
 	]
