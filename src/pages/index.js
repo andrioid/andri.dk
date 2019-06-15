3f362436311d3aa26fb5dfbbf85a224a550410dc
Andri Oskarsson <andri80@gmail.com>
2019-06-15 13:33:54 +0200

    giving up for now

--- src/pages/index.js
+++ src/pages/index.js
@@ -11,68 +11,65 @@ import { ILike } from '../components/ilike'
 // Colors: https://material.io/guidelines/style/color.html#color-color-palette "indigo"
 
 const IndexPage = ({ data }) => (
-	<div className="container mx-auto px-4">
-		<div />
-
-		<div>
-			<div>
+	<div className="flex flex-col font-sans min-h-screen text-gray-900">
+		<div className="items-center flex-col" style={{ border: '1px solid red' }}>
+			<div className="text-center">
 				<Img
 					resolutions={data.file.childImageSharp.resolutions}
-					className="h-16 w-16 rounded-full mx-auto "
+					className="block sm:w-12 mx-auto w-1/2"
 				/>
-				<h1>Hi! I'm Andri</h1>
-				<ILike
-					like={[
-						'React',
-						'Go',
-						'Docker',
-						'Kubernetes',
-						'Javascript',
-						'Networking',
-						'Cloud Computing',
-						'DadOps',
-						'LaTeX',
-						'PostgreSQL',
-						'GraphQL',
-						'Linux',
-						'GatsbyJS',
-						'React Native',
-						'UX',
-						'Product Development',
-						'Programming',
-						'DevOps',
-						'Arcade Games',
-						'Cooking',
-						'Coffee',
-						'Gadgets',
-						'Sci-Fi'
-					]}
-				/>
+				<h2 className="bg-yellow-400 text-2xl font-bold inline-block my-8 p-3">
+					Hi! I'm Andri
+				</h2>
 			</div>
 			<div>
-				<Github user="andrioid" />
-				<Twitter user="andrioid" />
-				<LinkedIn user="andriosk" />
+				<div className="max-w-sm mx-auto flex p-6 bg-white rounded-lg shadow-xl">
+					<div className="flex-shrink-0">
+						<img class="h-12 w-12" src={andratar} alt="ChitChat Logo" />
+					</div>
+					<div className="ml-6 pt-1">
+						<h4 className="text-xl text-gray-900 leading-tight">ChitChat</h4>
+						<p className="text-base text-gray-600 leading-normal">
+							You have a new message!
+						</p>
+					</div>
+				</div>
 			</div>
-			<div>
-				<i className="big chevron down icon" />
-			</div>
+			<ILike
+				like={[
+					'React',
+					'Go',
+					'Docker',
+					'Kubernetes',
+					'Javascript',
+					'Networking',
+					'Cloud Computing',
+					'DadOps',
+					'LaTeX',
+					'PostgreSQL',
+					'GraphQL',
+					'Linux',
+					'GatsbyJS',
+					'React Native',
+					'UX',
+					'Product Development',
+					'Programming',
+					'DevOps',
+					'Arcade Games',
+					'Cooking',
+					'Coffee',
+					'Gadgets',
+					'Sci-Fi'
+				]}
+			/>
 		</div>
-		<div
-			style={{
-				backgroundColor: '#1A237E',
-				margin: 0,
-				padding: 10,
-				justifyContent: 'center',
-				alignItems: 'center',
-				color: 'white',
-				textAlign: 'center'
-			}}
-		>
-			<p>
-				Damn it! You have discovered that this site contains no content. I will
-				have to fix that.
-			</p>
+		<div>
+			<Github user="andrioid" />
+			<Twitter user="andrioid" />
+			<LinkedIn user="andriosk" />
+		</div>
+		<div>
+			<i className="big chevron down icon" />
 		</div>
 	</div>
 )
