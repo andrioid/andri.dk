34c75e65fe8763f09cb5c421131a146f4355c313
Andri Oskarsson <andri80@gmail.com>
2019-06-15 11:09:37 +0200

    going to try to deploy this

--- src/pages/cv/index.js
+++ src/pages/cv/index.js
@@ -13,22 +13,16 @@ import Link from 'gatsby-link'
 
 import andriPic from '../../../static/img/coffee-art.jpg'
 
-import Defaultfont from '../../../static/fonts/Montserrat-Regular.otf'
-
-import HeaderFont from '../../../static/fonts/Montserrat-Regular.otf'
 import {
 	SectionHeader,
 	Headline,
 	Head,
 	ExperienceItem,
 	Box,
-	colors
-} from './elements'
-
-Font.register({
-	family: 'HeaderFont',
-	src: HeaderFont
-})
+	colors,
+	TimelineItem,
+	Paragraph
+} from '../../components/cv/elements'
 
 Font.register({
 	family: 'DefaultFont',
@@ -37,6 +31,10 @@ Font.register({
 		{
 			src: require('../../../static/fonts/Montserrat-SemiBold.otf'),
 			fontWeight: 700
+		},
+		{
+			src: require('../../../static/fonts/Montserrat-Italic.otf'),
+			fontStyle: 'italic'
 		}
 	]
 })
@@ -75,20 +73,28 @@ const MyDocument = () => (
 						<Text style={{ fontSize: 12 }}>Computer Engineer</Text>
 					</View>
 
-					<Box title="Frontend skills">
-						<Text>React, JavaScript, HTML, CSS, React Native</Text>
+					<Box title="Current Status">Not Seeking Employment</Box>
+
+					<Box title="Programming" color={colors.programming}>
+						<Text>
+							Frontend, Backend, Mobile, System Design, Message Queues, Git,
+							Subversion
+						</Text>
 					</Box>
-					<Box title="Backend skills">
-						<Text>Go, PHP, Node.js, MySQL, Postgres</Text>
+
+					<Box title="Operations" color={colors.operations}>
+						<Text>
+							Cloud Computing, Firebase, Linux, Monitoring, Backups, CI, CD,
+							Deployment, Docker
+						</Text>
 					</Box>
-					<Box title="DevOps skills" borderColor={colors.devops}>
-						<Text>Linux, Mac, Windows, Build systems, Cloud Computing</Text>
+
+					<Box title="Databases" color={colors.databases}>
+						<Text>MySQL, Postgres, MongoDB, Firestore, Redis, SQLite</Text>
 					</Box>
+
 					<Box title="Languages">
 						<Text>Icelandic (native), English (fluent), Danish (fluent)</Text>
-					</Box>
-					<Box title="Interests">
-						<Text>Technology, Photography, Walking, Basketball</Text>
 					</Box>
 
 					<Box title="Contact">
@@ -96,45 +102,108 @@ const MyDocument = () => (
 					</Box>
 				</View>
 				<View style={styles.right}>
+					<Box title="Profile">
+						<Text>
+							Driven, independent Software Developer with a flair for System
+							Administration
+						</Text>
+					</Box>
 					<Box title="Experience">
-						<ExperienceItem
+						<TimelineItem
 							title="Software Developer"
 							period="2019-Present"
 							employer="Redia A/S"
-							tags={['React', 'Firebase']}
+							tags={['React', 'Firebase', 'Firestore', 'TypeScript']}
 						>
-							Did stuff there, very important stuff. Nobody does more important
-							stuff than me
-						</ExperienceItem>
-						<ExperienceItem
+							Working on a custom platform with CMS features. Running entirely
+							on the Firebase platform.
+						</TimelineItem>
+						<TimelineItem
 							title="Tech Lead"
-							period="2010-2012"
+							period="2017-2019"
 							employer="PARKPARK A/S"
+							tags={[
+								'Linux',
+								'React',
+								'React Native',
+								'GCP',
+								'MySQL',
+								'JavaScript',
+								'PHP',
+								'DevOps'
+							]}
+						>
+							Pitched and developed our new app on both Android and iOS.
+							Migrated our production environment to Google Cloud and our
+							development to Docker.
+						</TimelineItem>
+						<TimelineItem
+							title="Senior Developer"
+							period="2015-2016"
+							employer="UVdata A/S (now KMD)"
+							tags={['JavaScript', 'React', 'HTML', 'CSS', 'DevOps']}
+						>
+							Worked on a green-field project with React and event-sourcing. On
+							our learning-platform I introduced React, did release management
+							and served as a Scrum master.
+						</TimelineItem>
+						<TimelineItem
+							title="IT Specialist"
+							period="2013-2015"
+							employer="YouSee A/S"
+							tags={['PHP', 'MySQL', 'Linux', 'DevOps']}
 						>
 							Did stuff there, very important stuff. Nobody does more important
 							stuff than me
-						</ExperienceItem>
-						<ExperienceItem
+						</TimelineItem>
+						<TimelineItem
 							title="Software Developer"
-							period="2010-2012"
-							employer="M1 A/S"
-							place="Ålborg"
-						>
-							Did stuff there, very important stuff. Nobody does more important
-							stuff than me
-						</ExperienceItem>
-						<ExperienceItem
+							employer="Lyngsø Systems A/S"
+							period="2013 - 2013"
+						/>
+						<TimelineItem
 							title="Software Developer"
-							period="2010-2012"
 							employer="M1 A/S"
-							place="Ålborg"
-						>
-							Did stuff there, very important stuff. Nobody does more important
-							stuff than me
-						</ExperienceItem>
+							period="2012 - 2013"
+						/>
+						<TimelineItem
+							title="Intern (Developer)"
+							employer="Sprettur ehf"
+							period="2012 - 2012"
+						/>
+						<Text style={{ fontStyle: 'italic', fontSize: 8 }}>
+							Been working in IT since 1999. Full list available on LinkedIn.
+						</Text>
 					</Box>
-					<SectionHeader>Experience</SectionHeader>
-					<SectionHeader>Education</SectionHeader>
+					<Box title="Education">
+						<TimelineItem
+							title="BSc, Computer Engineering"
+							period="2007-2012"
+							employer="Aalborg University"
+						/>
+						<TimelineItem
+							title="Agile Design & Test Driven Development"
+							period="2012"
+							employer="Sprettur ehf"
+						/>
+						<TimelineItem
+							title="Engineering Entrance Course"
+							period="2006-2007"
+							employer="Aalborg University"
+						/>
+					</Box>
+					<Box title="About">
+						<Paragraph>
+							I come from Reykjavík, Iceland and am the oldest of 5 brothers. In
+							2006 I moved to Denmark to study. I now live near Aalborg, Denmark
+							with my girlfriend and son. In my off-time I like to cook, take
+							photos and go for walks.
+						</Paragraph>
+						<Paragraph>
+							I've been facinated by computers since age 8 and spend every
+							waking minute learing some thing new about them.
+						</Paragraph>
+					</Box>
 				</View>
 			</View>
 		</Page>
@@ -147,17 +216,13 @@ const CV = () => (
 	</PDFViewer>
 )
 
-const CVFile = () => {
-	ReactPDF.render(<MyDocument />, `${__dirname}/example.pdf`)
-}
-
 const CVPage = () => {
-	const [showCV, setShowCV] = useState()
+	const [viewMode, setViewMode] = useState()
 	useEffect(() => {
-		setShowCV(true)
+		setViewMode('web')
 	}, [])
-	if (!showCV) {
-		return null
+	if (!viewMode) {
+		return <p>moo</p>
 	}
 	return (
 		<div style={{ display: 'flex', minHeight: '100vh' }}>
