0eeb35656fe518edc1971366a424177c1275ffe4
Andri Oskarsson <andri80@gmail.com>
2019-07-22 12:59:40 +0200

    svg header image

--- src/pages/index.js
+++ src/pages/index.js
@@ -15,46 +15,61 @@ import { Card } from '../components/card'
 
 const IndexPage = ({ data }) => (
 	<div className="">
-		<div className="flex flex-col font-sans md:min-h-one-third-screen text-white bg-blue-700">
-			<nav className="flex items-end justify-end justify-between items-center p-8">
-				<img
-					src={andratar}
-					className="rounded-full shadow-2xl w-16 h-16 md:invisible"
-				/>
-				<div>
-					<ul className="flex flex-row">
-						<NavLink href="blog/">Blog</NavLink>
-						<NavLink href="cv.pdf">CV</NavLink>
-					</ul>
-				</div>
-			</nav>
-			<div className="mx-10 md:mx-20 pb-8 lg:mx-40 flex flex-row flex-wrap font-headline text-2xl">
-				<div className="flex-1">
-					<h2 className="font-headline md:text-6xl text-3xl font-semibold inline-block my-2">
-						Hi, I'm Andri
-					</h2>
-					<div className="text-lg md:text-2xl">
-						<p>
-							Computer Engineer from &nbsp;
-							<span aria-hidden>
-								<ReactCountryFlag code="is" svg />
-							</span>
-							&nbsp;<span hidden>Iceland</span> living in &nbsp;
-							<ReactCountryFlag code="dk" svg />
-							<span hidden>Denmark</span>
-						</p>
-						<p>&nbsp;</p>
-						<p>
-							I make websites, create apps, manage infrastructure, develop
-							products and more.
-						</p>
-					</div>
-				</div>
-				<div className="hidden md:block p-4 items-start justify-start flex mr-6">
+		<div
+			className="flex flex-col font-sans md:min-h-one-third-screen text-white bg-blue-700 bg-fixed"
+			style={{
+				background: `url(${require('../../static/img/header.svg')})`,
+				backgroundRepeat: 'no-repeat',
+				backgroundSize: 'cover',
+				backgroundBlendMode: 'darken'
+			}}
+		>
+			<div
+				style={{
+					backgroundColor: 'rgba(0, 0, 0, 0.33)',
+					flex: 1
+				}}
+			>
+				<nav className="flex items-end justify-end justify-between items-center p-8">
 					<img
 						src={andratar}
-						className="rounded-full shadow-2xl block mx-auto md:w-48 md:h-48"
+						className="rounded-full shadow-2xl w-16 h-16 md:invisible"
 					/>
+					<div>
+						<ul className="flex flex-row">
+							<NavLink href="blog/">Blog</NavLink>
+							<NavLink href="cv.pdf">CV</NavLink>
+						</ul>
+					</div>
+				</nav>
+				<div className="mx-10 md:mx-20 pb-8 lg:mx-40 flex flex-row flex-wrap font-headline text-2xl">
+					<div className="flex-1">
+						<h2 className="font-headline md:text-6xl text-3xl font-semibold inline-block my-2">
+							Hi, I'm Andri
+						</h2>
+						<div className="text-lg md:text-2xl">
+							<p>
+								Computer Engineer from &nbsp;
+								<span aria-hidden>
+									<ReactCountryFlag code="is" svg />
+								</span>
+								&nbsp;<span hidden>Iceland</span> living in &nbsp;
+								<ReactCountryFlag code="dk" svg />
+								<span hidden>Denmark</span>
+							</p>
+							<p>&nbsp;</p>
+							<p>
+								I make websites, create apps, manage infrastructure, develop
+								products and more.
+							</p>
+						</div>
+					</div>
+					<div className="hidden md:block p-4 items-start justify-start flex mr-6">
+						<img
+							src={andratar}
+							className="rounded-full shadow-2xl block mx-auto md:w-48 md:h-48"
+						/>
+					</div>
 				</div>
 			</div>
 		</div>
