aabb35717224e9301313ef8ba3b21d4771b86b48
Andri Óskarsson <andri80@gmail.com>
2019-07-22 08:35:52 +0200

    wip

---
+++ src/layouts/pages.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const PagesLayout = ({ children }) => (
+	<div className="markdown">
+		<h1>Pages</h1>
+		{children}
+	</div>
+)
+
+export default PagesLayout
---
+++ src/layouts/posts.js
@@ -0,0 +1,5 @@
+import React from 'react'
+
+const PostsLayout = ({ children }) => <div>{children}</div>
+
+export default PostsLayout
