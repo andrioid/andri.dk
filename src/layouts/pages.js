8a88e1e94f53665f8934372ea1f8a0fa38f9b310
Andri Óskarsson <andri80@gmail.com>
2019-07-22 08:57:57 +0200

    wip

--- src/layouts/pages.js
+++ src/layouts/pages.js
@@ -1,10 +1,14 @@
 import React from 'react'
+import { Nav } from '../components/nav'
 
 const PagesLayout = ({ children }) => (
-	<div className="markdown">
-		<h1>Pages</h1>
-		{children}
-	</div>
+	<>
+		<Nav />
+		<div className="markdown">
+			<h1>Pages</h1>
+			{children}
+		</div>
+	</>
 )
 
 export default PagesLayout
