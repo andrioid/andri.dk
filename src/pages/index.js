3a5713d19207afe1a84f4abb02f71d9ec62a0662
Andri Oskarsson <andri80@gmail.com>
2019-07-23 14:25:38 +0200

    tweaking the body container

--- src/pages/index.js
+++ src/pages/index.js
@@ -40,7 +40,6 @@ const IndexPage = ({ data }) => (
 							<NavLink href="blog/">Blog</NavLink>
 
 							<NavLink href="now/">Now</NavLink>
-							<NavLink href="cv.pdf">CV</NavLink>
 						</ul>
 					</div>
 				</nav>
@@ -150,7 +149,9 @@ const NavLink = ({ href, children }) => (
 
 // Wraps the text and handles margins
 export const BodyContainer = ({ children, className }) => (
-	<div className="mt-10 mx-10 md:mx-20 lg:mx-40 text-xl">{children}</div>
+	<div className="mt-10 mx-5 md:mx-10 md:mx-20 lg:mx-40 text-xl md:max-w-4xl">
+		{children}
+	</div>
 )
 
 const Section = ({
