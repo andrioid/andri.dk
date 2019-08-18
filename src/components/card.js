6f3ebc2f80e1373231ac423769a89da9c71ba88b
Andri Oskarsson <andri80@gmail.com>
2019-08-18 07:17:57 +0200

    Card fix

--- src/components/card.js
+++ src/components/card.js
@@ -17,37 +17,38 @@ export const Card = ({
 	tags = [],
 	draft = false
 }) => (
-	<div className="pt-2 sm:pt-4 sm:pr-4 lg:pr-6 w-full sm:w-1/2 xl:w-1/3">
-		<Link to={link}>
-			<div className="osverflow-hidden shadow-lg bg-white hover:shadow-2xl h-auto md:h-350 h-350">
-				<div className="flex flex-col px-6 pb-4 bg-green-200 justify-between h-full">
-					<div className="mt-4 text-sm text-gray-600 flex justify-start items-center">
-						<FaCalendar />
-						<span className="ml-2">{date}</span>
-					</div>
-
-					<div className="flex mt-1 font-bold text-lg lg:text-xl mb-2">
-						{title}
-					</div>
-					<div className="flex flex-1 text-gray-700 text-base bg-pink-200 h-20">
-						<p className="">{description}</p>
-					</div>
-					{tags.length > 0 ? (
-						<div className="bg-purple-200 px-6 py-4">
-							{tags.slice(0, 3).map(t => (
-								<span key={t} className="tag">
-									{t}
-								</span>
-							))}
-							{draft ? (
-								<span className="tag text-white bg-red-400">Draft</span>
-							) : null}
-						</div>
-					) : null}
+	<Link
+		className="flex pt-2 sm:pt-4 sm:pr-4 lg:pr-6 w-full sm:w-1/2 xl:w-1/3"
+		to={link}
+	>
+		<div className="shadow-lg bg-white hover:shadow-2xl text-gray-900">
+			<div className="flex flex-col px-6 pb-4 justify-between h-full">
+				<div className="mt-4 text-sm text-gray-600 flex justify-start items-center">
+					<FaCalendar />
+					<span className="ml-2">{date}</span>
 				</div>
 
-				<div className="px-6 py-4"></div>
+				<div className="flex mt-1 font-bold text-lg lg:text-xl mb-2">
+					{title}
+				</div>
+				<div className="flex flex-1 text-base h-20">
+					<p className="">{description}</p>
+				</div>
+				{tags.length > 0 ? (
+					<div className="py-4">
+						{tags.slice(0, 3).map(t => (
+							<span key={t} className="tag">
+								{t}
+							</span>
+						))}
+						{draft ? (
+							<span className="tag text-white bg-red-400">Draft</span>
+						) : null}
+					</div>
+				) : null}
 			</div>
-		</Link>
-	</div>
+
+			<div className="px-6 py-4"></div>
+		</div>
+	</Link>
 )
