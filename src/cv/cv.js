9bec4bd4b0ac28b23468289a5eb40f3536139ce2
Andri Óskarsson <andri80@gmail.com>
2019-11-17 15:11:44 +0100

    wip

--- src/cv/cv.js
+++ src/cv/cv.js
@@ -1,167 +1,176 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from "react";
 import ReactPDF, {
-	Page,
-	Text,
-	View,
-	Document,
-	StyleSheet,
-	PDFViewer,
-	Image,
-	Font,
-	Link
-} from '@react-pdf/renderer'
+  Page,
+  Text,
+  View,
+  Document,
+  StyleSheet,
+  PDFViewer,
+  Image,
+  Font,
+  Link
+} from "@react-pdf/renderer";
 
 import {
-	SectionHeader,
-	Headline,
-	Head,
-	ExperienceItem,
-	Box,
-	colors,
-	TimelineItem,
-	Paragraph,
-	EducationItem,
-	periodToString
-} from './elements'
+  SectionHeader,
+  Headline,
+  Head,
+  ExperienceItem,
+  Box,
+  colors,
+  TimelineItem,
+  Paragraph,
+  EducationItem,
+  periodToString
+} from "./elements";
 
-import resolveConfig from 'tailwindcss/resolveConfig'
+import resolveConfig from "tailwindcss/resolveConfig";
 
-import localConfig from '../../tailwind.config'
+import localConfig from "../../tailwind.config";
 
-const { theme } = resolveConfig(localConfig)
+const { theme } = resolveConfig(localConfig);
 
-import resume from './resume.json'
+import resume from "./resume.json";
 
-import { Andri } from './pictures'
-
-// Create styles
-const styles = StyleSheet.create({
-	page: {
-		paddingVertical: 30,
-		paddingHorizontal: 30,
-		fontSize: 9,
-		color: theme.colors.gray[900],
-		fontFamily: 'DefaultFont'
-	},
-	leftHeader: {
-		alignItems: 'center',
-		paddingBottom: 20
-	},
-	left: {
-		width: '30vw',
-		marginRight: 20
-	},
-	right: {
-		marginLeft: 20,
-		flex: 1
-	}
-})
+import { Andri } from "./pictures";
 
 const CVFrontpage = ({ image }) => (
-	<Page size="A4" style={styles.page}>
-		<View style={{ flexDirection: 'row' }}>
-			<View style={styles.left}>
-				<View wrap={false} style={styles.leftHeader}>
-					<Head src={Andri} />
-					<Headline>Andri Óskarsson</Headline>
-					<Text style={{ fontSize: 12 }}>Computer Engineer</Text>
-				</View>
+  <Page size="A4" style={styles.page}>
+    <View style={styles.top}>
+      <Head src={Andri} />
+      <View style={{ marginLeft: 20 }}>
+        <Text style={{ fontSize: 20, fontWeight: "semibold" }}>
+          Andri Óskarsson
+        </Text>
+        <Text style={{ fontSize: 12 }}>Computer Engineer</Text>
+      </View>
+    </View>
+    <View
+      style={{
+        marginTop: 5,
+        paddingVertical: 10,
+        borderTop: 0.5,
+        borderBottom: 0.5,
+        borderColor: "#d3d3d3",
+        backgroundColor: colors.programming
+      }}
+    >
+      <Text
+        style={{
+          fontStyle: "italic",
+          fontSize: 12,
+          textAlign: "center",
+          color: "white"
+        }}
+      >
+        {resume.basics.summary}
+      </Text>
+    </View>
 
-				<Box title="Programming" color={colors.programming}>
-					<Text>
-						Frontend, Backend, Mobile, System Design, Message Queues, Git,
-						Subversion
-					</Text>
-				</Box>
+    <View
+      style={{
+        flexDirection: "row"
+      }}
+    >
+      <View style={styles.left}>
+        <Box title="Education">
+          {resume.education.map(e => (
+            <EducationItem
+              key={`${e.institution + e.startDate}`}
+              institution={e.institution}
+              area={e.area}
+              studyType={e.studyType}
+              startDate={e.startDate}
+              endDate={e.endDate}
+            />
+          ))}
+        </Box>
 
-				<Box title="Operations" color={colors.operations}>
-					<Text>
-						Cloud Computing, Firebase, Linux, Monitoring, Backups, CI, CD,
-						Deployment, Docker
-					</Text>
-				</Box>
+        <Box title="Languages">
+          <Text>Icelandic (native)</Text>
+          <Text>English (fluent)</Text>
+          <Text>Danish (fluent)</Text>
+        </Box>
 
-				<Box title="Databases" color={colors.databases}>
-					<Text>MySQL, Postgres, MongoDB, Firestore, Redis, SQLite</Text>
-				</Box>
+        <Box title="Social">
+          <Text>Co-organizer Aalborg React Meetup</Text>
+          <Text>Co-organizer Aalborg Hackathon</Text>
+        </Box>
 
-				<Box title="Languages">
-					<Text>Icelandic (native)</Text>
-					<Text>English (fluent)</Text>
-					<Text>Danish (fluent)</Text>
-				</Box>
+        <Box title="Contact">
+          <Link src="mailto:m@andri.dk">
+            <Text>m@andri.dk</Text>
+          </Link>
+        </Box>
+      </View>
+      <View wrap={true} style={styles.right}>
+        <View>
+          <SectionHeader>Experience</SectionHeader>
+          {resume.work.slice(0, 100).map(w => (
+            <TimelineItem
+              key={`${w.company + w.startDate}`}
+              title={w.position}
+              employer={w.company}
+              period={periodToString(w.startDate, w.endDate)}
+              tags={w.skills}
+            >
+              {w.summary}
+            </TimelineItem>
+          ))}
+        </View>
 
-				<Box title="Social">
-					<Text>Co-organizer Aalborg React Meetup</Text>
-					<Text>Co-organizer Aalborg Hackathon</Text>
-				</Box>
-
-				<Box title="Contact">
-					<Link src="mailto:m@andri.dk">
-						<Text>m@andri.dk</Text>
-					</Link>
-				</Box>
-			</View>
-			<View style={styles.right}>
-				<View
-					style={{
-						marginBottom: 10,
-						backgroundColor: theme.colors.gray[100],
-						padding: 4
-					}}
-				>
-					<SectionHeader>Profile</SectionHeader>
-					<Text>{resume.basics.summary}</Text>
-				</View>
-				<View>
-					<SectionHeader>Experience</SectionHeader>
-					{resume.work.slice(0, 100).map(w => (
-						<TimelineItem
-							key={`${w.company + w.startDate}`}
-							title={w.position}
-							employer={w.company}
-							period={periodToString(w.startDate, w.endDate)}
-							tags={w.skills}
-						>
-							{w.summary}
-						</TimelineItem>
-					))}
-				</View>
-
-				<View>
-					<SectionHeader>Education</SectionHeader>
-					{resume.education.map(e => (
-						<EducationItem
-							key={`${e.institution + e.startDate}`}
-							institution={e.institution}
-							area={e.area}
-							studyType={e.studyType}
-							startDate={e.startDate}
-							endDate={e.endDate}
-						/>
-					))}
-				</View>
-
-				<Box title="About">
-					<Paragraph>
-						I come from Reykjavík, Iceland and am the oldest of 5 brothers. In
-						2006 I moved to Denmark to study. I now live near Aalborg, Denmark
-						with my girlfriend and son. In my off-time I like to cook, take
-						photos and go for walks.
-					</Paragraph>
-					<Paragraph>
-						I've been facinated by computers since age 8 and spend much of my
-						free time, learning more about them.
-					</Paragraph>
-				</Box>
-			</View>
-		</View>
-	</Page>
-)
+        <Box title="About">
+          <Paragraph>
+            I come from Reykjavík, Iceland and am the oldest of 5 brothers. In
+            2006 I moved to Denmark to study. I now live near Aalborg, Denmark
+            with my girlfriend and son. In my off-time I like to cook, take
+            photos and go for walks.
+          </Paragraph>
+          <Paragraph>
+            I've been facinated by computers since age 8 and spend much of my
+            free time, learning more about them.
+          </Paragraph>
+        </Box>
+      </View>
+    </View>
+  </Page>
+);
 
 // Create Document Component
 export const CVDoc = ({ image }) => (
-	<Document>
-		<CVFrontpage image={image} />
-	</Document>
-)
+  <Document>
+    <CVFrontpage image={image} />
+  </Document>
+);
+
+// Create styles
+const styles = StyleSheet.create({
+  page: {
+    paddingVertical: 30,
+    paddingHorizontal: 30,
+    //marginHorizontal: 30,
+    fontSize: 8,
+    color: theme.colors.gray[900],
+    fontFamily: "DefaultFont"
+  },
+  leftHeader: {
+    alignItems: "center",
+    paddingBottom: 20
+  },
+  top: {
+    flexDirection: "row",
+    alignItems: "center"
+  },
+  left: {
+    width: "50%",
+    marginTop: 20,
+    paddingRight: 10
+    //flex: 1,
+  },
+  right: {
+    marginTop: 20,
+    paddingLeft: 10,
+    width: "50%"
+    //flex: 1
+  }
+});
--- src/cv/elements.js
+++ src/cv/elements.js
@@ -1,200 +1,197 @@
-import React from 'react'
+import React from "react";
 
 import ReactPDF, {
-	Page,
-	Text,
-	View,
-	Document,
-	StyleSheet,
-	PDFViewer,
-	Image,
-	Font
-} from '@react-pdf/renderer'
+  Page,
+  Text,
+  View,
+  Document,
+  StyleSheet,
+  PDFViewer,
+  Image,
+  Font
+} from "@react-pdf/renderer";
 
-import { hex } from 'wcag-contrast'
-import dayjs from 'dayjs'
-import 'dayjs/locale/en'
+import { hex } from "wcag-contrast";
+import dayjs from "dayjs";
+import "dayjs/locale/en";
 
 export const colors = {
-	borders: '#d3d3d3',
-	operations: '#259490',
-	programming: '#2460A7',
-	databases: '#491D70'
-}
+  borders: "#d3d3d3",
+  operations: "#259490",
+  programming: "#2460A7",
+  databases: "#491D70"
+};
 
 export const tagColors = {
-	react: colors.programming,
-	linux: colors.operations,
-	gcp: colors.operations,
-	mysql: colors.databases,
-	firestore: colors.databases,
-	php: colors.programming,
-	typescript: colors.programming,
-	javascript: colors.programming
-}
+  react: colors.programming,
+  linux: colors.operations,
+  gcp: colors.operations,
+  mysql: colors.databases,
+  firestore: colors.databases,
+  php: colors.programming,
+  typescript: colors.programming,
+  javascript: colors.programming
+};
 
 export const Head = ({ src }) => (
-	<Image
-		source={src}
-		resizeMode="contain"
-		style={{
-			width: 100,
-			height: 100,
-			borderRadius: 50,
-			marginBottom: 10,
-			borderColor: colors.borders,
-			borderWidth: 1
-		}}
-	/>
-)
+  <Image
+    source={src}
+    resizeMode="contain"
+    style={{
+      width: 75,
+      height: 75,
+      borderRadius: 50,
+      marginBottom: 10,
+      borderColor: colors.borders,
+      borderWidth: 1
+    }}
+  />
+);
 
-export const SectionHeader = ({ children, color = 'black' }) => {
-	const first = children.toUpperCase()
-	const last = children.substring(1)
-	return (
-		<>
-			<Text style={{ fontWeight: 'bold' }}>
-				<Text style={{ color: color }}>{first}</Text>
-			</Text>
-			<View
-				style={{
-					width: 'auto',
-					height: 1,
-					borderBottomColor: colors.borders,
-					borderBottomWidth: 0.5,
-					marginBottom: 10
-				}}
-			/>
-		</>
-	)
-}
+export const SectionHeader = ({ children, color = "black" }) => {
+  const first = children.toUpperCase();
+  const last = children.substring(1);
+  return (
+    <>
+      <Text style={{ fontWeight: "bold", fontSize: 12 }}>
+        <Text style={{ color: color }}>{first}</Text>
+      </Text>
+      <View
+        style={{
+          width: "auto",
+          height: 1,
+          marginBottom: 5
+        }}
+      />
+    </>
+  );
+};
 
 export const Headline = ({ children }) => (
-	<Text style={{ fontWeight: 'bold', fontSize: 18, marginBottom: 5 }}>
-		{children}
-	</Text>
-)
+  <Text style={{ fontWeight: "bold", fontSize: 15, marginBottom: 5 }}>
+    {children}
+  </Text>
+);
 
 export const TimelineItem = ({
-	title,
-	period,
-	children,
-	employer,
-	tags = [],
-	location
+  title,
+  period,
+  children,
+  employer,
+  tags = [],
+  location
 }) => {
-	tags = tags.sort()
-	return (
-		<View wrap={false} style={{ marginBottom: 10 }}>
-			<View
-				style={{
-					flexDirection: 'row',
-					justifyContent: 'space-between',
-					marginBottom: 2.5,
-					flexWrap: 'wrap'
-				}}
-			>
-				<Text style={{ fontWeight: 'bold' }}>
-					{title}, <Text style={{ fontWeight: 'normal' }}>{employer}</Text>
-				</Text>
-				<Text>{period}</Text>
-			</View>
+  tags = tags.sort();
+  return (
+    <View wrap={false} style={{ marginTop: 5 }}>
+      <View
+        style={{
+          flexDirection: "row",
+          //justifyContent: "space-between",
+          marginBottom: 2.5,
+          flexWrap: "wrap"
+        }}
+      >
+        <Text style={{ fontWeight: "bold" }}>
+          {title}, <Text style={{ fontWeight: "normal" }}>{employer}</Text>
+        </Text>
+      </View>
 
-			{children && <Text style={{ marginBottom: 2.5 }}>{children}</Text>}
-			{tags && (
-				<View style={{ flexDirection: 'row' }}>
-					{tags &&
-						tags.map(m => (
-							<Tag key={m} color={tagColors[m.toLowerCase()]}>
-								{m}
-							</Tag>
-						))}
-				</View>
-			)}
-		</View>
-	)
-}
+      {children && <Text style={{ marginBottom: 2.5 }}>{children}</Text>}
+      {tags && (
+        <View wrap style={{ flexDirection: "row", flexWrap: "wrap" }}>
+          {tags &&
+            tags.map(m => (
+              <Tag key={m} color={tagColors[m.toLowerCase()]}>
+                {m}
+              </Tag>
+            ))}
+        </View>
+      )}
+    </View>
+  );
+};
 
 export const EducationItem = ({
-	institution,
-	startDate,
-	endDate,
-	area,
-	studyType
+  institution,
+  startDate,
+  endDate,
+  area,
+  studyType
 }) => (
-	<View wrap={false} style={{ marginBottom: 10 }}>
-		<View
-			style={{
-				flexDirection: 'row',
-				justifyContent: 'space-between',
-				marginBottom: 2.5
-			}}
-		>
-			<Text style={{ fontWeight: 'bold' }}>
-				{area}, {studyType}
-			</Text>
-			<Text>
-				{startDate} - {endDate}
-			</Text>
-		</View>
-		<Text style={{ fontWeight: 'normal' }}>{institution}</Text>
-	</View>
-)
+  <View wrap={false} style={{ marginTop: 5 }}>
+    <View
+      style={{
+        flexDirection: "row",
+        justifyContent: "space-between"
+      }}
+    >
+      <Text style={{ fontWeight: "bold" }}>
+        {area}, {studyType}
+      </Text>
+      <Text>
+        {startDate} - {endDate}
+      </Text>
+    </View>
+    <Text style={{ fontWeight: "normal" }}>{institution}</Text>
+  </View>
+);
 
 export const Tag = ({ color = colors.borders, children }) => (
-	<View
-		style={{
-			borderRadius: 2.5,
-			borderWidth: 0.5,
-			borderColor: color,
-			marginRight: 2.5,
-			marginVertical: 2.5,
-			padding: 2.5,
-			backgroundColor: color
-		}}
-	>
-		<Text
-			style={{
-				fontSize: 6,
-				fontWeight: 'bold',
-				color: hex('#00000', color) < 10 ? 'white' : 'black'
-			}}
-		>
-			{children}
-		</Text>
-	</View>
-)
+  <View
+    style={{
+      borderRadius: 2.5,
+      borderWidth: 0.5,
+      borderColor: color,
+      marginRight: 2.5,
+      marginVertical: 2.5,
+      padding: 2.5,
+      borderColor: color
+    }}
+  >
+    <Text
+      style={{
+        fontSize: 6,
+        fontWeight: "bold",
+        //color: hex("#00000", color) < 10 ? "white" : "black"
+        color: "black"
+      }}
+    >
+      {children}
+    </Text>
+  </View>
+);
 
 // Not meant for anything wrapping pages
 export const Box = ({ children, title, color, style = {} }) => (
-	<View wrap={false} style={{ marginBottom: 20 }}>
-		<SectionHeader color={color}>{title}</SectionHeader>
-		<View style={{ ...style }}>
-			{children && typeof children === 'string' ? (
-				<Text>{children}</Text>
-			) : (
-				children
-			)}
-		</View>
-	</View>
-)
+  <View wrap={false} style={{ marginBottom: 10 }}>
+    <SectionHeader color={color}>{title}</SectionHeader>
+    <View style={{ ...style }}>
+      {children && typeof children === "string" ? (
+        <Text>{children}</Text>
+      ) : (
+        children
+      )}
+    </View>
+  </View>
+);
 
 export const Paragraph = ({ children }) => (
-	<Text style={{ marginBottom: 5 }}>{children}</Text>
-)
+  <Text style={{ marginBottom: 5 }}>{children}</Text>
+);
 
 export const periodToString = (startDate, endDate) => {
-	const start = dayjs(startDate)
-	const end = dayjs(endDate)
-	const dateformat = 'YYYY'
+  const start = dayjs(startDate);
+  const end = dayjs(endDate);
+  const dateformat = "YYYY";
 
-	if (endDate === startDate) {
-		return `${start.format(dateformat)}`
-	}
+  if (endDate === startDate) {
+    return `${start.format(dateformat)}`;
+  }
 
-	if (!endDate) {
-		return `${start.format(dateformat)} - Present`
-	}
+  if (!endDate) {
+    return `${start.format(dateformat)} - Present`;
+  }
 
-	return `${start.format(dateformat)} - ${end.format(dateformat)}`
-}
+  return `${start.format(dateformat)} - ${end.format(dateformat)}`;
+};
