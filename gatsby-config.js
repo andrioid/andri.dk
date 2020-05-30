9a5473e5cc09aacce50c53e54ee7aedfbd5fadcc
Andri Oskarsson <andri80@gmail.com>
2020-05-30 09:38:27 +0200

    switched to npm, moved social-cards plugin to its own repo

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,18 +9,18 @@ module.exports = {
         : "http://localhost:8000",
     author: "Andri Óskarsson",
     social: {
-      twitter: "andrioid"
-    }
+      twitter: "andrioid",
+    },
   },
   plugins: [
     {
-      resolve: require.resolve("./plugins/social-card"),
+      resolve: "gatsby-plugin-social-cards",
       options: {
         authorImage: "./static/img/coffee-art.jpg",
         backgroundImage: "./static/img/hvitserkur.JPG",
         defaultAuthor: "Andri Óskarsson",
-        design: "default"
-      }
+        design: "default",
+      },
     },
     {
       resolve: `gatsby-plugin-feed`,
@@ -40,13 +40,13 @@ module.exports = {
         feeds: [
           {
             serialize: ({ query: { site, allMarkdownRemark } }) => {
-              return allMarkdownRemark.edges.map(edge => {
+              return allMarkdownRemark.edges.map((edge) => {
                 return Object.assign({}, edge.node.frontmatter, {
                   description: edge.node.excerpt,
                   date: edge.node.frontmatter.date,
                   url: site.siteMetadata.siteUrl + edge.node.frontmatter.path,
                   guid: site.siteMetadata.siteUrl + edge.node.frontmatter.path,
-                  custom_elements: [{ "content:encoded": edge.node.html }]
+                  custom_elements: [{ "content:encoded": edge.node.html }],
                 });
               });
             },
@@ -75,16 +75,16 @@ module.exports = {
             // if `string` is used, it will be used to create RegExp and then test if pathname of
             // current page satisfied this regular expression;
             // if not provided or `undefined`, all pages will have feed reference inserted
-            match: "^/blog/"
-          }
-        ]
-      }
+            match: "^/blog/",
+          },
+        ],
+      },
     },
     {
       resolve: "gatsby-plugin-draft",
       options: {
-        publishDraft: process.env.NODE_ENV !== "production"
-      }
+        publishDraft: process.env.NODE_ENV !== "production",
+      },
     },
     "gatsby-plugin-postcss",
     `gatsby-transformer-sharp`,
@@ -94,29 +94,29 @@ module.exports = {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `pages`,
-        path: `${__dirname}/src/pages/`
-      }
+        path: `${__dirname}/src/pages/`,
+      },
     },
     {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `blog`,
-        path: `${__dirname}/src/blog/`
-      }
+        path: `${__dirname}/src/blog/`,
+      },
     },
     {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `img`,
-        path: `${__dirname}/static/img/`
-      }
+        path: `${__dirname}/static/img/`,
+      },
     },
     "gatsby-transformer-json", // Resume JSON
     {
       resolve: "gatsby-source-filesystem",
       options: {
-        path: "./src/cv"
-      }
+        path: "./src/cv",
+      },
     },
 
     `gatsby-plugin-react-helmet`,
@@ -126,8 +126,8 @@ module.exports = {
       options: {
         trackingId: "UA-1081252-4",
         // Setting this parameter is optional
-        anonymize: true
-      }
+        anonymize: true,
+      },
     },
     {
       resolve: "gatsby-transformer-remark",
@@ -135,21 +135,21 @@ module.exports = {
         plugins: [
           {
             resolve: "gatsby-remark-prismjs",
-            options: {}
+            options: {},
           },
           {
             resolve: "gatsby-remark-images",
             //tracedSVG: true,
             options: {
               maxWidth: 896,
-              wrapperStyle: "max-width: 896px;"
-            }
+              wrapperStyle: "max-width: 896px;",
+            },
           },
           {
-            resolve: "gatsby-remark-copy-linked-files"
-          }
-        ]
-      }
-    }
-  ]
+            resolve: "gatsby-remark-copy-linked-files",
+          },
+        ],
+      },
+    },
+  ],
 };
