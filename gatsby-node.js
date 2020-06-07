7eb265395ae750f83714840dba8933ddf2b4a904
Andri Oskarsson <andri80@gmail.com>
2020-06-07 16:00:57 +0200

    use npm run for build-cv

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,6 +1,6 @@
-const path = require('path')
-const cp = require('child_process')
-const { createFilePath } = require('gatsby-source-filesystem')
+const path = require("path");
+const cp = require("child_process");
+const { createFilePath } = require("gatsby-source-filesystem");
 
 // exports.onCreateWebpackConfig = ({ getConfig, actions }) => {
 // 	const oldConfig = getConfig()
@@ -16,56 +16,56 @@ const { createFilePath } = require('gatsby-source-filesystem')
 // }
 
 exports.createPages = async ({ actions, graphql }) => {
-	const { createPage } = actions
-	const blogPostTemplate = path.resolve(`src/layouts/blog-post.js`)
-	const result = await graphql(`
-		{
-			allMarkdownRemark(
-				sort: { order: DESC, fields: [frontmatter___date] }
-				limit: 1000
-			) {
-				edges {
-					node {
-						excerpt(pruneLength: 250)
-						html
-						id
-						frontmatter {
-							date
-							path
-							title
-						}
-					}
-				}
-			}
-		}
-	`)
+  const { createPage } = actions;
+  const blogPostTemplate = path.resolve(`src/layouts/blog-post.js`);
+  const result = await graphql(`
+    {
+      allMarkdownRemark(
+        sort: { order: DESC, fields: [frontmatter___date] }
+        limit: 1000
+      ) {
+        edges {
+          node {
+            excerpt(pruneLength: 250)
+            html
+            id
+            frontmatter {
+              date
+              path
+              title
+            }
+          }
+        }
+      }
+    }
+  `);
 
-	if (result.errors) {
-		console.log(result.errors)
-		throw new Error('Things broke, see console output above')
-	}
+  if (result.errors) {
+    console.log(result.errors);
+    throw new Error("Things broke, see console output above");
+  }
 
-	result.data.allMarkdownRemark.edges.forEach(({ node }) => {
-		createPage({
-			path: node.frontmatter.path,
-			component: blogPostTemplate,
-			context: {}
-		})
-	})
-}
+  result.data.allMarkdownRemark.edges.forEach(({ node }) => {
+    createPage({
+      path: node.frontmatter.path,
+      component: blogPostTemplate,
+      context: {},
+    });
+  });
+};
 
 exports.onCreateNode = ({ node, getNode, actions }) => {
-	const { createNodeField } = actions
-	if (node.internal.type === `MarkdownRemark`) {
-		const slug = createFilePath({ node, getNode, basePath: `pages` })
-		createNodeField({
-			node,
-			name: `slug`,
-			value: slug
-		})
-	}
-}
+  const { createNodeField } = actions;
+  if (node.internal.type === `MarkdownRemark`) {
+    const slug = createFilePath({ node, getNode, basePath: `pages` });
+    createNodeField({
+      node,
+      name: `slug`,
+      value: slug,
+    });
+  }
+};
 
 exports.onPostBuild = () => {
-	cp.execSync('yarn run build-cv')
-}
+  cp.execSync("npm run build-cv");
+};
