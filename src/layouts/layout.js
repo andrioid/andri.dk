9f131789d475c5c640a379bcc53a50471539a09e
Andri Óskarsson <andri80@gmail.com>
2020-06-18 17:24:03 +0200

    npm upgrades, yay

--- src/layouts/layout.js
+++ src/layouts/layout.js
@@ -1,30 +1,26 @@
 import React from 'react'
-import { StaticQuery, graphql } from 'gatsby'
+import { useStaticQuery, graphql } from 'gatsby'
 import Helmet from 'react-helmet'
 
-export const Layout = ({ slug, children }) => (
-	<StaticQuery
-		query={graphql`
-			query SiteTitleQuery {
-				site {
-					siteMetadata {
-						title
-						siteUrl
-					}
-				}
+export const Layout = ({ slug, children }) => {
+	const data = useStaticQuery(graphql`
+	query SiteTitleQuery {
+		site {
+			siteMetadata {
+				title
+				siteUrl
 			}
-		`}
-		render={data => (
-			<>
-				<Helmet title={data.site.siteMetadata.title}>
-					<meta name="twitter:card" content="summary_large_image" />
-					<meta
-						name="twitter:image"
-						content={`${data.site.siteMetadata.siteUrl}${slug}twitter-card.jpg`}
-					/>
-				</Helmet>
-				{children}
-			</>
-		)}
-	/>
-)
+		}
+	}
+`)
+	return <>
+		<Helmet title={data.site.siteMetadata.title}>
+			<meta name="twitter:card" content="summary_large_image" />
+			<meta
+				name="twitter:image"
+				content={`${data.site.siteMetadata.siteUrl}${slug}twitter-card.jpg`}
+			/>
+		</Helmet>
+		{children}
+	</>
+}
