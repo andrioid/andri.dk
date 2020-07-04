131ffa790569e88561186bb25c48aa3e36f71568
Andri Óskarsson <andri80@gmail.com>
2020-07-04 09:39:59 +0200

    try to prevent netlify from redirecting

--- gatsby-node.js
+++ gatsby-node.js
@@ -66,6 +66,23 @@ exports.onCreateNode = ({ node, getNode, actions }) => {
   }
 };
 
+// Replacing '/' would result in empty string which is invalid
+const replacePath = path => (path === `/` ? path : path.replace(/\/$/, ``))
+// Implement the Gatsby API “onCreatePage”. This is
+// called after every page is created.
+exports.onCreatePage = ({ page, actions }) => {
+  const { createPage, deletePage } = actions
+
+  const oldPage = Object.assign({}, page)
+  // Remove trailing slash unless page is /
+  page.path = replacePath(page.path)
+  if (page.path !== oldPage.path) {
+    // Replace new page with old page
+    deletePage(oldPage)
+    createPage(page)
+  }
+}
+
 exports.onPostBuild = () => {
   cp.execSync("npm run build-cv");
 };
