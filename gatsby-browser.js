79077985deca2a0ca3625d2eefc158489a66ca3e
Andri <andri@andri.dk>
2022-04-30 09:26:15 +0200

    cleaning up old stuff

--- gatsby-browser.js
+++
@@ -1,6 +0,0 @@
-import "typeface-montserrat";
-import "typeface-source-code-pro";
-import "prismjs/themes/prism-tomorrow.css";
-import "prismjs/plugins/line-numbers/prism-line-numbers.css";
-import "prismjs/plugins/command-line/prism-command-line.css";
-import "./src/css/global.css";
