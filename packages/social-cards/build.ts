a97280709f3aaf275ca60eeaa8fe0337847f7134
Andri <andri@andri.dk>
2022-06-25 11:20:43 +0200

    bleh

--- packages/social-cards/build.ts
+++ packages/social-cards/build.ts
@@ -1,29 +1,32 @@
 import { Resvg, ResvgRenderOptions } from "@resvg/resvg-js";
 import { Options, Result } from "./types";
-import { promises } from "node:fs";
+import { promises, existsSync } from "node:fs";
 import { resolve } from "node:path";
 
-export const fontPath = resolve("../social-cards", "fonts");
+export const fontPath = resolve("static", "fonts");
 
 export const defaultRsvgOptions: Partial<ResvgRenderOptions> = {
-	imageRendering: 1,
 	logLevel: "debug",
 	font: {
-		loadSystemFonts: false,
-		fontDirs: [fontPath],
+		loadSystemFonts: true,
+		fontDirs: ["static/fonts"],
+		defaultFontFamily: "Montserrat",
 	},
 };
 
-const defaultOptions: Partial<Options> = {
+export const defaultOptions: Partial<Options> = {
 	resvg: defaultRsvgOptions,
+	outputDir: "public/social-cards",
+	urlPath: "/",
+	publicDir: "public",
 };
 
 export async function generateImage(options: Options): Promise<Result> {
 	options = { ...defaultOptions, ...options };
+	await promises.writeFile(resolve("./test-out-ori.svg"), options.svg);
 	const resvg = new Resvg(options.svg, options.resvg);
 	const pngData = resvg.render();
 	const pngBuffer = pngData.asPng();
-	await promises.writeFile(resolve("./test-out-ori.svg"), options.svg);
 	await promises.writeFile(resolve("./test-out.svg"), resvg.toString());
 	await promises.writeFile(resolve("./test-out.png"), pngBuffer);
 
@@ -32,4 +35,20 @@ export async function generateImage(options: Options): Promise<Result> {
 		path: "moo",
 		hash: "omo",
 	};
+}
+
+export function validateImage(image: string): string {
+	if (image.indexOf("/") === 0) {
+		image = image.substring(1);
+	}
+
+	const imagePath = resolve(".", image);
+	//console.log("Validating", image, imagePath);
+
+	if (!existsSync(imagePath)) {
+		throw new Error(
+			"[social-cards] image defined, but does not exist: " + imagePath
+		);
+	}
+	return imagePath;
 }
--- packages/social-cards/templates/popup-card/popup-card.ts
+++ packages/social-cards/templates/popup-card/popup-card.ts
@@ -1,48 +1,60 @@
 import { Options } from "../../types";
 import { resolve } from "node:path";
-import { defaultRsvgOptions } from "../../build";
+import { defaultOptions, defaultRsvgOptions, validateImage } from "../../build";
+import { existsSync } from "node:fs";
 
 export function PopupCard({
 	title = "default title",
 	subtitle = "default subtitle",
-	authorImageBase64,
+	authorImage,
+	backgroundImage,
 }: {
 	title: string;
 	subtitle?: string;
-	authorImageBase64?: string;
+	authorImage?: string;
+	backgroundImage?: string;
 }): Options {
 	const iwidth = 400;
 	const iheight = 200;
 	const xMargin = 40;
 	const authorWidth = 32;
 
-	let texty = 170;
+	let texty = 162;
 	if (!subtitle) {
 		texty = texty + 5;
 	}
-	const imageGroup = authorImageBase64
-		? `
-	<g stroke="2" clipPath="url(#clip)">
-	<rect
-	  id="rect"
-	  width=${authorWidth}
-	  height=${authorWidth}
-	  x=${iwidth - authorWidth - xMargin}
-	  y="156"
-	  fill="none"
-	  stroke="#2b6cb0"
-	  strokeWidth="1"
-	  rx="50"
-	/>
-	<image
-	  width=${authorWidth}
-	  height=${authorWidth}
-	  x=${iwidth - authorWidth - xMargin}
-	  y="156"
-	  xlinkHref=${authorImageBase64}
-	/>
-	</g>`
-		: "";
+	let authorImageSvg = "";
+	if (authorImage) {
+		authorImage = validateImage(authorImage);
+		console.log("authorimage", authorImage);
+		authorImageSvg = `
+	<g stroke="2" clip-path="url(#clip)">
+		<image
+			width="${authorWidth}"
+			height="${authorWidth}"
+			x="${iwidth - authorWidth - xMargin}"
+			y="156"
+			xlink:href="${authorImage}"
+		/>
+	</g>`;
+	}
+
+	let backgroundImageSvg = "";
+	if (backgroundImage) {
+		backgroundImage = validateImage(backgroundImage);
+		backgroundImageSvg = `
+			<image
+				width="${iwidth}"
+				height="${iheight}"
+				x="0"
+				y="0"
+				xlink:href="${backgroundImage}"
+				preserveAspectRatio="xMidYMid slice"
+				transform="scale(1,1)"
+			/>
+		
+		`;
+	}
 
 	let svg = "";
 	svg += `
@@ -50,51 +62,67 @@ export function PopupCard({
 		width="1200"
 		height="600"
 		viewBox="0 0 ${iwidth} ${iheight}"
-		font-family="exotica"
 		font-size="12"
 		version="1.1"
 		xmlns="http://www.w3.org/2000/svg"
-		xmlnsXlink="http://www.w3.org/1999/xlink"
+		xmlns:xlink="http://www.w3.org/1999/xlink"
 	>
 		<defs>
 			<clipPath id="clip">
-			<use xlinkHref="#rect" />
+				<use xlink:href="#authorImageRect" />
 			</clipPath>
+			<rect
+				id="authorImageRect"
+				width="${authorWidth}"
+				height="${authorWidth}"
+				x="${iwidth - authorWidth - xMargin}"
+				y="156"
+				fill="none"
+				rx="50"
+			/>
+	  
 			<filter id="shadow" asx="0" asy="0">
-			<feGaussianBlur in="SourceAlpha" stdDeviation="3" />
-			<feOffset dx="0.5" dy="-0.5" />
-			<feMerge>
-				<feMergeNode />
-				<feMergeNode in="SourceGraphic" />
-			</feMerge>
+				<feGaussianBlur in="SourceAlpha" stdDeviation="3" />
+				<feOffset dx="0.5" dy="-0.5" />
+				<feMerge>
+					<feMergeNode />
+					<feMergeNode in="SourceGraphic" />
+				</feMerge>
 			</filter>
 		</defs>
 
+		${backgroundImageSvg}
+
 		<g filter="url(#shadow)">
 			<rect
-			width="360"
-			height="120"
-			x="20"
-			y="140"
-			rx="10"
-			style="fill: #fff; strokeWidth: 0;"
+				width="360"
+				height="120"
+				x="20"
+				y="140"
+				rx="10"
+				style="fill: #fff; stroke-width: 0;"
 			/>
 		</g>
-		<g class="texts" style="fill: #000; font-size: 12;">
-			<text x="${xMargin}" y="${texty}">
+
+		${authorImageSvg}
+
+		<g class="texts" fill="#000" font-size="10" >
+			<text x="${xMargin}" y="${texty}" font-weight="bold">
 				${title}
 			</text>
-			<text x="${xMargin}" y="${texty + 15}" style="font-size: 10; fill: #a3a3a3;">
+			<text x="${xMargin + 2}" y="${
+		texty + 10
+	}" style="font-size: 6; fill: #a3a3a3;" font-family="Pacifico">
 				${subtitle}
 			</text>
+			<g font-size="7" font-weight="bold">
+				<text x="${xMargin}" y="${texty + 24}">#React</text>
+			</g>
 		</g>
 	</svg>		
 	`;
 
 	return {
 		svg,
-		resvg: {
-			...defaultRsvgOptions,
-		},
-	};
+	} as Options;
 }
