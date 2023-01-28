3e72e3316ef9280f70a6eac00f8138199243b1e1
Andri <andri@andri.dk>
2023-01-28 12:37:30 +0100

    new header

--- packages/site/src/components/MainBody.tsx
+++ packages/site/src/components/MainBody.tsx
@@ -1,17 +1,19 @@
 import * as React from "react";
 import waveBG from "../../static/img/wave.svg";
+import "./weird.css";
 
 export const MainBody = ({ children }) => (
 	<body>
-		<div
-			className="flex flex-col font-sans md:min-h-one-third-screen text-white bg-blue-700"
-			style={{
-				background: `url(${waveBG}) no-repeat`,
-				backgroundSize: "cover",
-				//backgroundImage: `url(${waveBG})`,
-			}}
-		>
-			{children}
+		<div>
+			<div
+				className="flex flex-col font-sans md:min-h-one-third-screen text-white bg-blue-900 animated-background"
+				style={{
+					//background: `url(${waveBG}) no-repeat`,
+					backgroundSize: "cover",
+				}}
+			>
+				{children}
+			</div>
 		</div>
 	</body>
 );
