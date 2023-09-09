6c3903d90e9695f9a98f5328e4171529e8d3328d
Andri <andri@andri.dk>
2023-09-09 14:40:27 +0200

    renaming site

--- src/constants.ts
+++ src/constants.ts
@@ -3,7 +3,7 @@ export const site = {
 		import.meta.env.MODE === "development"
 			? "http://localhost:3000"
 			: "https://andri.dk",
-	title: "andri.dk",
+	title: "Andri Óskarsson | andri.dk",
 	description:
 		"I make websites, create apps, manage infrastructure, develop products and more.",
 	author: "Andri Óskarsson",
