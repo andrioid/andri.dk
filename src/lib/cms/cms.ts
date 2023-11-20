ae3e507a9b27cddc9f0ed11a9fc3a1fd9bed4cd5
Andri Óskarsson <account+github@andri.dk>
2023-11-20 12:50:45 +0100

    cms test

---
+++ src/lib/cms/cms.ts
@@ -0,0 +1,16 @@
+import { createDirectus, readItems, rest, staticToken } from "@directus/sdk"
+import type { CollectionEntry } from "astro:content"
+
+type Schema = {
+    blog: Array<{
+        title?: string
+    }>
+}
+
+export function getDirectusClient() {
+    const URL = import.meta.env.DIRECTUS_URL
+    const TOKEN = import.meta.env.DIRECTUS_TOKEN
+    const directus = createDirectus<Schema>(URL).with(rest()).with(staticToken(TOKEN))
+    return directus
+}
+
