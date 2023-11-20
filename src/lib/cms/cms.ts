27c3bd0f55ad4804e1cfb0e3c115c95f4e70d612
Andri Óskarsson <account+github@andri.dk>
2023-11-20 17:09:05 +0100

    somehow i broke the build again

--- src/lib/cms/cms.ts
+++ src/lib/cms/cms.ts
@@ -2,14 +2,14 @@ import { createDirectus, readItems, rest, staticToken } from "@directus/sdk";
 import type { CollectionEntry } from "astro:content";
 import type { DirectusBlog } from "./blog";
 
-const URL = import.meta.env.DIRECTUS_URL ?? process.env.DIRECTUS_URL;
-const TOKEN = import.meta.env.DIRECTUS_TOKEN ?? process.env.DIRECTUS_TOKEN;
-
 type Schema = {
 	blog: Array<DirectusBlog>;
 };
 
 export function getDirectusClient() {
+	const URL = import.meta.env.DIRECTUS_URL ?? process.env.DIRECTUS_URL;
+	const TOKEN = import.meta.env.DIRECTUS_TOKEN ?? process.env.DIRECTUS_TOKEN;
+
 	if (!URL || !TOKEN) {
 		console.log("URL/TOKEN", URL, TOKEN);
 		throw new Error("Missing CMS URL and/or TOKEN");
