baa9960c583b88a785ab6ef37def292da6a067ca
Andri Óskarsson <account+github@andri.dk>
2024-09-29 07:57:46 +0200

    my default layout plugin was doing inception stuff on local blog pages

--- src/lib/remark-default-layout/index.ts
+++ src/lib/remark-default-layout/index.ts
@@ -4,19 +4,23 @@ import type { CommonBlog } from "../cms";
 // This sets our default layout even if there is no frontmatter
 // - Use case: Simple pages like /now or /using
 interface AstroFile extends VFile {
-	data: {
-		astro: {
-			frontmatter: CommonBlog & {
-				layout?: string;
-			};
-		};
-	};
+  data: {
+    astro: {
+      frontmatter: CommonBlog & {
+        layout?: string;
+      };
+    };
+  };
 }
 
 export const setLayout = () => {
-	const transformer = (_, file: AstroFile) => {
-		file.data.astro.frontmatter.layout =
-			file.data.astro.frontmatter.layout ?? "@layouts/MdPageLayout.astro";
-	};
-	return transformer;
+  const transformer = (_, file: AstroFile) => {
+    if (file.data.astro.frontmatter.slug) {
+      // Not a file, but a loaded resource
+      return;
+    }
+    file.data.astro.frontmatter.layout =
+      file.data.astro.frontmatter.layout ?? "@layouts/MdPageLayout.astro";
+  };
+  return transformer;
 };
