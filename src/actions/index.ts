8c36a2d6e143405b4ac75ee72f5ca0bf4b202dc4
Andri Óskarsson <account+github@andri.dk>
2024-10-14 09:22:24 +0200

    server actions, lets go

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -2,14 +2,24 @@ import { defineAction } from "astro:actions";
 import { z } from "astro:schema";
 
 export const server = {
-	contactMe: defineAction({
-		accept: "form",
-		input: z.object({
-			email: z.string().email(),
-		}),
-		handler: async (input) => {
-			console.log("EMAIL YEAH" + input.email);
-			return `Hello, ${input.email}!`;
-		},
-	}),
+  contactMe: defineAction({
+    accept: "form",
+    input: z.object({
+      email: z.string().email(),
+    }),
+    handler: async (input) => {
+      console.log("EMAIL YEAH" + input.email);
+      return `Hello, ${input.email}!`;
+    },
+  }),
+  getGreeting: defineAction({
+    input: z.object({
+      name: z.string(),
+    }),
+    handler: async (input, ctx) => {
+      // https://discord.com/channels/830184174198718474/1295282295510405202
+      // TODO: Revisit this. Can't find the IP in the Astro request
+      return `Hello ${input.name}`;
+    },
+  }),
 };
