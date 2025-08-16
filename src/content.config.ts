196e1f31692e39069d9ae7738ce592b145c36d54
Andri Óskarsson <account+github@andri.dk>
2025-08-16 08:15:52 +0200

    fixing css and adding social to blog content schema

--- src/content.config.ts
+++ src/content.config.ts
@@ -2,21 +2,26 @@ import { glob } from "astro/loaders";
 import { defineCollection, z } from "astro:content";
 
 const blogCollection = defineCollection({
-	loader: glob({
-		pattern: ["**/*.md", "**/*.mdx"],
-		base: "./src/content/blog",
-	}),
-	schema: z.object({
-		title: z.string(),
-		// TODO: use z.coerce.date() ?
-		date: z.string().transform((str) => new Date(str)),
-		draft: z.boolean().optional(),
-		tags: z.array(z.string()).optional(),
-		description: z.string().optional(),
-		coverImage: z.string().optional(),
-	}),
+  loader: glob({
+    pattern: ["**/*.md", "**/*.mdx"],
+    base: "./src/content/blog",
+  }),
+  schema: z.object({
+    title: z.string(),
+    // TODO: use z.coerce.date() ?
+    date: z.string().transform((str) => new Date(str)),
+    draft: z.boolean().optional(),
+    tags: z.array(z.string()).optional(),
+    description: z.string().optional(),
+    coverImage: z.string().optional(),
+    social: z
+      .object({
+        bluesky: z.string().optional(),
+      })
+      .optional(),
+  }),
 });
 
 export const collections = {
-	blog: blogCollection,
+  blog: blogCollection,
 };
