ded39254721b0c92eb95975aa7e8a1748df0841c
Andri Óskarsson <account+github@andri.dk>
2025-09-02 08:20:05 +0200

    unified static dir for blog

--- src/lib/cv/pdf-utils.ts
+++ src/lib/cv/pdf-utils.ts
@@ -1,67 +1,67 @@
 import { Font, renderToBuffer } from "@react-pdf/renderer";
 
-import path from "node:path";
 import fs from "node:fs";
+import path from "node:path";
+import { createElement } from "react";
 import { DEFAULT_FONT, Resume } from "./";
 import { colors } from "./colors";
-import { createElement } from "react";
 import { CVWrapper } from "./react/cv-wrapper";
 
 // PDF can only be generated server-side (import doesnt work well with this)
-export const STATIC_DIR = path.join(process.cwd(), "static");
+export const STATIC_DIR = path.join(process.cwd(), "public");
 
 function fixFontImport(imp: string): string {
-  const out = path.join(STATIC_DIR, "fonts/pdf", imp);
-  if (!fs.existsSync(out)) {
-    throw new Error("Font not found: " + out);
-  }
-  return out;
+	const out = path.join(STATIC_DIR, "fonts/pdf", imp);
+	if (!fs.existsSync(out)) {
+		throw new Error("Font not found: " + out);
+	}
+	return out;
 }
 
 export function registerFonts() {
-  Font.registerHyphenationCallback((word) => [word]);
+	Font.registerHyphenationCallback((word) => [word]);
 
-  try {
-    Font.register({
-      family: DEFAULT_FONT,
-      fonts: [
-        {
-          src: fixFontImport("Montserrat-Regular.ttf"),
-        },
-        {
-          src: fixFontImport("Montserrat-SemiBold.ttf"),
-          fontWeight: 700,
-        },
-        {
-          src: fixFontImport("Montserrat-Light.ttf"),
-          fontWeight: 300,
-        },
-        {
-          src: fixFontImport("Montserrat-Italic.ttf"),
-          fontStyle: "italic",
-        },
-      ],
-    });
+	try {
+		Font.register({
+			family: DEFAULT_FONT,
+			fonts: [
+				{
+					src: fixFontImport("Montserrat-Regular.ttf"),
+				},
+				{
+					src: fixFontImport("Montserrat-SemiBold.ttf"),
+					fontWeight: 700,
+				},
+				{
+					src: fixFontImport("Montserrat-Light.ttf"),
+					fontWeight: 300,
+				},
+				{
+					src: fixFontImport("Montserrat-Italic.ttf"),
+					fontStyle: "italic",
+				},
+			],
+		});
 
-    Font.register({
-      family: "Brands",
-      src: fixFontImport("brands.ttf"),
-    });
-    Font.register({
-      family: "Awesome",
-      src: fixFontImport("awesome-solid.ttf"),
-    });
-  } catch (err) {
-    console.error("WTF");
-  }
+		Font.register({
+			family: "Brands",
+			src: fixFontImport("brands.ttf"),
+		});
+		Font.register({
+			family: "Awesome",
+			src: fixFontImport("awesome-solid.ttf"),
+		});
+	} catch (err) {
+		console.error("WTF");
+	}
 }
 
 export async function resumeToString(resume: Resume) {
-  registerFonts();
-  const cvDoc = createElement(CVWrapper, { resume: resume });
-  return renderToBuffer(cvDoc as any);
+	registerFonts();
+	const cvDoc = createElement(CVWrapper, { resume: resume });
+	return renderToBuffer(cvDoc as any);
 }
 
 export function getColor(name: keyof typeof colors | string): string {
-  return colors[name as keyof typeof colors];
+	return colors[name as keyof typeof colors];
 }
--- src/lib/cv/react/cv-document.tsx
+++ src/lib/cv/react/cv-document.tsx
@@ -1,199 +1,210 @@
 import * as ReactPDF from "@react-pdf/renderer";
-import type { Basics, Education, Skill, Work } from "../types";
-import { Icon } from "./icon";
-import { Head } from "./head";
-//import andratar from "@static/img/coffee-art.jpg";
-import { Section } from "./section";
-import { EducationItem } from "./education-item";
-import { Paragraph } from "./paragraph";
-import { SkillKeywords } from "./skills-keywords";
 import { DEFAULT_FONT } from "..";
 import { colors } from "../colors";
-import { Profiles } from "./profiles";
+import type { Basics, Education, Skill, Work } from "../types";
+import { EducationItem } from "./education-item";
 import { Experience } from "./experience";
 import { Footer } from "./footer";
+import { Head } from "./head";
+import { Icon } from "./icon";
+import { Paragraph } from "./paragraph";
+import { Profiles } from "./profiles";
+import { Section } from "./section";
+import { SkillKeywords } from "./skills-keywords";
 
 const { Link, Page, StyleSheet, Text, Document, View } = ReactPDF;
 
 export const CVDocument = ({
-  basics,
-  education,
-  work,
-  skills,
+	basics,
+	education,
+	work,
+	skills,
 }: {
-  basics: Basics;
-  education: Education[];
-  work: Work[];
-  skills: Skill[];
+	basics: Basics;
+	education: Education[];
+	work: Work[];
+	skills: Skill[];
 }) => {
-  const idxFirstSpace = basics.name.indexOf(" ");
-  const firstName = basics.name.substring(0, idxFirstSpace + 1);
-  const lastName = basics.name.substring(idxFirstSpace + 1);
+	const idxFirstSpace = basics.name.indexOf(" ");
+	const firstName = basics.name.substring(0, idxFirstSpace + 1);
+	const lastName = basics.name.substring(idxFirstSpace + 1);
 
-  return (
-    <Document author={basics.name} subject={`${basics.name} CV`}>
-      <Page size="A4" style={styles.page}>
-        <View style={styles.top}>
-          <View>
-            <Text style={styles.textName}>
-              <Text style={styles.firstName}>{firstName}</Text>
-              <Text style={styles.lastName}>{lastName}</Text>
-            </Text>
-            <Text style={styles.textTitle}>{basics.label}</Text>
-            <Text style={styles.location}>
-              <Icon name="home" />
-              &nbsp;&nbsp;Aalborg, Denmark
-            </Text>
-          </View>
-          <Head />
-        </View>
+	return (
+		<Document author={basics.name} subject={`${basics.name} CV`}>
+			<Page size="A4" style={styles.page}>
+				<View style={styles.top}>
+					<View>
+						<Text style={styles.textName}>
+							<Text style={styles.firstName}>{firstName}</Text>
+							<Text style={styles.lastName}>{lastName}</Text>
+						</Text>
+						<Text style={styles.textTitle}>{basics.label}</Text>
+						<Text style={styles.location}>
+							<Icon name="home" />
+							&nbsp;&nbsp;Aalborg, Denmark
+						</Text>
+					</View>
+					<Head />
+				</View>
 
-        <View
-          style={{
-            flexDirection: "row",
-          }}
-        >
-          <View style={styles.left}>
-            <Section title="Education" iconName="education">
-              {education?.map((e) => (
-                <EducationItem
-                  item={e}
-                  key={`${e.institution + e.startDate}`}
-                />
-              ))}
-            </Section>
-            {basics.summary && (
-              <Section title="About" iconName="about">
-                <Paragraph style={{ marginTop: 0 }}>{basics.summary}</Paragraph>
-              </Section>
-            )}
-            <Section title="Skills" iconName="skills">
-              <Text
-                style={{
-                  color: "grey",
-                  fontSize: 7,
-                  marginTop: -10,
-                }}
-              >
-                Ordered by experience. Underline for preference.
-              </Text>
-              {skills?.map((s) => (
-                <View key={s.name} wrap={true} style={{ marginTop: 10 }}>
-                  <Text
-                    style={{
-                      fontWeight: "bold",
-                      color:
-                        s.id !== undefined
-                          ? colors[s.id as keyof typeof colors]
-                          : "black",
-                    }}
-                    key={s.name}
-                  >
-                    {s.name}
-                  </Text>
-                  <View
-                    style={{
-                      flexWrap: "wrap",
-                      flexDirection: "row",
-                      marginTop: 5,
-                    }}
-                  >
-                    <SkillKeywords skill={s} />
-                  </View>
-                </View>
-              ))}
-            </Section>
-            <Section title="Languages" iconName="language">
-              <Text>Icelandic (native)</Text>
-              <Text>English (fluent)</Text>
-              <Text>Danish (fluent)</Text>
-            </Section>
-            <Section
-              title="Networking"
-              style={{
-                backgroundColor: colors["grayBackground"],
-              }}
-            >
-              <Text>
-                Speaker & Co-Organizer{" "}
-                <Link src="https://meetup.com">Aalborg React Meetup</Link>
-              </Text>
-            </Section>
-            <Profiles profiles={basics.profiles!}></Profiles>
-          </View>
-          <View style={styles.right}>
-            <Experience work={work} skills={skills} />
-          </View>
-        </View>
+				<View
+					style={{
+						flexDirection: "row",
+					}}
+				>
+					<View style={styles.left}>
+						<Section title="Education" iconName="education">
+							{education?.map((e) => (
+								<EducationItem
+									item={e}
+									key={`${e.institution + e.startDate}`}
+								/>
+							))}
+						</Section>
+						{basics.summary && (
+							<Section title="About" iconName="about">
+								<Paragraph style={{ marginTop: 0 }}>
+									{basics.summary}
+								</Paragraph>
+							</Section>
+						)}
+						<Section title="Skills" iconName="skills">
+							<Text
+								style={{
+									color: "grey",
+									fontSize: 7,
+									marginTop: -10,
+								}}
+							>
+								Ordered by experience. Underline for preference.
+							</Text>
+							{skills?.map((s) => (
+								<View
+									key={s.name}
+									wrap={true}
+									style={{ marginTop: 10 }}
+								>
+									<Text
+										style={{
+											fontWeight: "bold",
+											color:
+												s.id !== undefined
+													? colors[
+															s.id as keyof typeof colors
+														]
+													: "black",
+										}}
+										key={s.name}
+									>
+										{s.name}
+									</Text>
+									<View
+										style={{
+											flexWrap: "wrap",
+											flexDirection: "row",
+											marginTop: 5,
+										}}
+									>
+										<SkillKeywords skill={s} />
+									</View>
+								</View>
+							))}
+						</Section>
+						<Section title="Languages" iconName="language">
+							<Text>Icelandic (native)</Text>
+							<Text>English (fluent)</Text>
+							<Text>Danish (fluent)</Text>
+						</Section>
+						<Section
+							title="Networking"
+							style={{
+								backgroundColor: colors["grayBackground"],
+							}}
+						>
+							<Text>
+								Speaker & Co-Organizer{" "}
+								<Link src="https://meetup.com">
+									Aalborg React Meetup
+								</Link>
+							</Text>
+						</Section>
+						<Profiles profiles={basics.profiles!}></Profiles>
+					</View>
+					<View style={styles.right}>
+						<Experience work={work} skills={skills} />
+					</View>
+				</View>
 
-        <Section title="References">
-          <Text style={{ fontStyle: "italic" }}>Available on request...</Text>
-        </Section>
+				<Section title="References">
+					<Text style={{ fontStyle: "italic" }}>
+						Available on request...
+					</Text>
+				</Section>
 
-        <Footer email={basics.email!} name={"CV"} />
-      </Page>
-    </Document>
-  );
+				<Footer email={basics.email!} name={"CV"} />
+			</Page>
+		</Document>
+	);
 };
 
 // Create styles
 const styles = StyleSheet.create({
-  page: {
-    //paddingVertical: 30,
-    position: "relative",
-    paddingTop: 20,
-    paddingBottom: 60,
-    paddingHorizontal: 30,
-    //marginHorizontal: 30,
-    fontSize: 8,
-    color: "black",
-    fontFamily: DEFAULT_FONT,
-  },
-  leftHeader: {
-    //alignItems: "center",
-    paddingBottom: 20,
-  },
-  top: {
-    flexDirection: "row",
-    //alignItems: "center",
-    paddingBottom: 20,
-    paddingTop: 20,
-    paddingLeft: 10,
-    justifyContent: "space-between",
-  },
-  topSummary: {
-    marginTop: 20,
-    paddingVertical: 10,
-    borderRadius: 2.5,
-  },
-  topSummaryText: {
-    fontSize: 11,
-    fontWeight: "semibold",
-    //color: "white",
-  },
-  left: {
-    //width: "50%",
-    //marginTop: 20,
-    paddingRight: 10,
-    backgroundColor: colors.grayBackground,
-    borderRadius: 2.5,
-    flex: 1.4,
-  },
-  right: {
-    //marginTop: 20,
-    paddingLeft: 10,
-    //width: "50%",
-    flex: 2,
-  },
-  textName: { fontSize: 30, fontWeight: 500 },
-  firstName: {},
-  lastName: {},
-  textTitle: {
-    marginTop: 0,
-    fontSize: 12,
-    color: colors.personLabel,
-  },
-  location: {
-    marginTop: 5,
-  },
+	page: {
+		//paddingVertical: 30,
+		position: "relative",
+		paddingTop: 20,
+		paddingBottom: 60,
+		paddingHorizontal: 30,
+		//marginHorizontal: 30,
+		fontSize: 8,
+		color: "black",
+		fontFamily: DEFAULT_FONT,
+	},
+	leftHeader: {
+		//alignItems: "center",
+		paddingBottom: 20,
+	},
+	top: {
+		flexDirection: "row",
+		//alignItems: "center",
+		paddingBottom: 20,
+		paddingTop: 20,
+		paddingLeft: 10,
+		justifyContent: "space-between",
+	},
+	topSummary: {
+		marginTop: 20,
+		paddingVertical: 10,
+		borderRadius: 2.5,
+	},
+	topSummaryText: {
+		fontSize: 11,
+		fontWeight: "semibold",
+		//color: "white",
+	},
+	left: {
+		//width: "50%",
+		//marginTop: 20,
+		paddingRight: 10,
+		backgroundColor: colors.grayBackground,
+		borderRadius: 2.5,
+		flex: 1.4,
+	},
+	right: {
+		//marginTop: 20,
+		paddingLeft: 10,
+		//width: "50%",
+		flex: 2,
+	},
+	textName: { fontSize: 30, fontWeight: 500 },
+	firstName: {},
+	lastName: {},
+	textTitle: {
+		marginTop: 0,
+		fontSize: 12,
+		color: colors.personLabel,
+	},
+	location: {
+		marginTop: 5,
+	},
 });
