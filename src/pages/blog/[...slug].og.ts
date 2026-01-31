ade842efb59a25eabaa78b6501cacd2df80ea9d4
Andri Óskarsson <account+github@andri.dk>
2026-01-31 08:10:21 +0100

    fixing 404 handling

--- src/pages/blog/[...slug].og.ts
+++ src/pages/blog/[...slug].og.ts
@@ -1,6 +1,5 @@
 // Note: Does not work with Deno SSR yet
 import { Params } from "astro";
-import { CollectionEntry } from "astro:content";
 import { getPost } from "~/lib/cms";
 import { codesnippetFromMarkdown } from "./_cmp/code-snippet-from-md";
 import { codesnippetResponse } from "./_cmp/code-snippet-response";
@@ -8,22 +7,28 @@ import { ogCardResponse } from "./_cmp/og-card-response";
 import { takumiRenderer } from "./_cmp/takumi";
 
 export async function GET({ params }: { params: Params }) {
-	const { slug } = params;
-	if (!slug) {
-		throw new Error("Missing param slug");
-	}
-	const post: CollectionEntry<"blog"> = await getPost(slug);
+  const { slug } = params;
+  if (!slug) {
+    throw new Error("Missing param slug");
+  }
+  const post = await getPost(slug);
+  if (!post) {
+    return new Response(null, {
+      status: 404,
+      statusText: "Not found",
+    });
+  }
 
-	// If most of the content is a single code snippet, then use it as the og-image
-	const snippet = codesnippetFromMarkdown(post.body ?? "");
-	if (snippet && post.body) {
-		if (snippet[0].code.length > post.body.length * 0.5) {
-			return await codesnippetResponse({
-				code: snippet[0].code,
-				lang: snippet[0].lang,
-				renderer: takumiRenderer,
-			});
-		}
-	}
-	return ogCardResponse({ post, renderer: takumiRenderer });
+  // If most of the content is a single code snippet, then use it as the og-image
+  const snippet = codesnippetFromMarkdown(post.body ?? "");
+  if (snippet && post.body) {
+    if (snippet[0].code.length > post.body.length * 0.5) {
+      return await codesnippetResponse({
+        code: snippet[0].code,
+        lang: snippet[0].lang,
+        renderer: takumiRenderer,
+      });
+    }
+  }
+  return ogCardResponse({ post, renderer: takumiRenderer });
 }
