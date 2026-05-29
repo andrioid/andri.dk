2b71a5aa9a8dd2cbb101745cab79018e62647856
Andri Óskarsson <m+git@andri.dk>
2026-05-29 15:09:45 +0200

    cv was too frontend focused, fixed it

--- src/lib/cv/react/experience.tsx
+++ src/lib/cv/react/experience.tsx
@@ -3,50 +3,48 @@ import { Section } from "./section";
 import { WorkItem } from "./work-item";
 
 export function Experience({
-	work,
-	skills,
-	fullYears = 8,
+  work = [],
+  skills,
 }: {
-	work?: Work[];
-	skills?: Skill[];
-	fullYears?: number;
+  work?: Work[];
+  skills?: Skill[];
 }) {
-	const latestWork = work?.filter((w) => filterLatest(w, false, fullYears));
-	const olderWork = work?.filter((w) => filterLatest(w, true, fullYears));
-
-	return (
-		<Section wrap={true} title="Experience" iconName="work">
-			{latestWork?.slice(0, 100).map((w, idx) => (
-				<WorkItem
-					item={w}
-					idx={idx}
-					key={`${w.company + w.startDate}`}
-					skills={skills}
-				/>
-			))}
-			{olderWork?.map((w) => {
-				return (
-					<WorkItem
-						item={w}
-						key={`${w.company + w.startDate}`}
-						short
-					/>
-				);
-			})}
-		</Section>
-	);
+  const fullWork = work.sort(
+    (a, b) => new Date(b.startDate).getTime() - new Date(a.startDate).getTime(),
+  );
+  return (
+    <Section wrap={true} title="Experience" iconName="work">
+      {fullWork?.map((w, idx) => (
+        <WorkItem
+          item={w}
+          idx={idx}
+          key={`${w.company + w.startDate}`}
+          skills={skills}
+          short={shouldCollapse(w)}
+        />
+      ))}
+    </Section>
+  );
 }
 
-function filterLatest(w: Work, reverse = false, years: number = 5) {
-	if (!w.endDate) {
-		return reverse ? false : true;
-	}
-	const date = new Date(w.endDate);
-	if (
-		date.getTime() >
-		new Date().getTime() - 1000 * 60 * 60 * 24 * 365 * years
-	) {
-		return reverse ? false : true;
-	}
-	return reverse ? true : false;
+function shouldCollapse(w: Work): boolean {
+  const now = new Date();
+  const start = new Date(w.startDate);
+  const end = w.endDate ? new Date(w.endDate) : new Date();
+  const numberOfMonthsAtJob =
+    (end.getTime() - start.getTime()) / (1000 * 60 * 60 * 24 * 30);
+  const numberOfYearsSince =
+    (now.getTime() - end.getTime()) / (1000 * 60 * 60 * 24 * 365);
+
+  // Keep recent (except short stints)
+  if (numberOfYearsSince < 10 && numberOfMonthsAtJob >= 12) {
+    return false;
+  }
+
+  // Still show jobs I've held for over 2 years
+  if (numberOfMonthsAtJob >= 24) {
+    return false;
+  }
+
+  return true;
 }
--- src/lib/cv/react/profiles.tsx
+++ src/lib/cv/react/profiles.tsx
@@ -1,56 +1,78 @@
 import { Link, Text, View } from "@react-pdf/renderer";
 import { URL } from "url";
+import { Profile } from "../types";
 import { Icon } from "./icon";
 import { Section } from "./section";
-import { Profile } from "../types";
 
-export function Profiles({ profiles }: { profiles: Profile[] }) {
-	return (
-		<Section title="Profiles">
-			{profiles?.map((profile, idx) => {
-				const key = `${profile.network}/${profile.username}`;
-				return (
-					<View
-						key={key}
-						style={{
-							marginBottom: idx < profiles!.length ? 5 : 0,
-						}}
-					>
-						<View
-							style={{
-								flexDirection: "row",
-							}}
-						>
-							<Icon name={profile.network.toLowerCase() as any} />
-							<View
-								style={{
-									marginLeft: 10,
-									width: 30,
-								}}
-							>
-								<Text>{profile.network}</Text>
-							</View>
-							<Text
-								style={{
-									marginLeft: 10,
-									fontStyle: "italic",
-								}}
-							>
-								{profile.username}
-							</Text>
-						</View>
-						<Text
-							style={{
-								color: "grey",
-							}}
-						>
-							<Link src={profile.url}>{`${
-								new URL(profile.url).hostname
-							}${new URL(profile.url).pathname}`}</Link>
-						</Text>
-					</View>
-				);
-			})}
-		</Section>
-	);
+const cvNetworks = ["github", "linkedin"];
+
+export function Profiles({ profiles = [] }: { profiles: Profile[] }) {
+  const filteredProfiles = profiles.filter((p) => {
+    if (cvNetworks.includes(p.network.toLowerCase())) return true;
+    return false;
+  });
+
+  return (
+    <Section title="Find me">
+      {filteredProfiles.map((profile, idx) => {
+        const key = `${profile.network}/${profile.username}`;
+        return (
+          <View
+            key={key}
+            style={{
+              marginBottom: idx < profiles!.length ? 5 : 0,
+            }}
+          >
+            <View
+              style={{
+                flexDirection: "row",
+              }}
+            >
+              <Icon name={profile.network.toLowerCase() as any} />
+              <View
+                style={{
+                  marginLeft: 10,
+                  width: 30,
+                }}
+              >
+                <Text>{profile.network}</Text>
+              </View>
+              <Text
+                style={{
+                  marginLeft: 10,
+                  fontStyle: "italic",
+                }}
+              >
+                {profile.username}
+              </Text>
+            </View>
+            <Text
+              style={{
+                color: "grey",
+              }}
+            >
+              <Link src={profile.url}>{`${
+                new URL(profile.url).hostname
+              }${new URL(profile.url).pathname}`}</Link>
+            </Text>
+          </View>
+        );
+      })}
+      <View
+        style={{
+          flexDirection: "row",
+        }}
+      >
+        <Icon name={"home"} />
+        <View
+          style={{
+            marginLeft: 10,
+          }}
+        >
+          <Text>Personal page</Text>
+        </View>
+      </View>
+      <Link src="https://andri.dk">andri.dk</Link>
+    </Section>
+  );
 }
